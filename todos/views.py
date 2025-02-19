50f46340b690d7b943ca4fd5c39e4dbe1f3f2c7e
nicky-nacky <n.abarinov@yandex.ru>
2025-02-19 21:07:58 +0300

    make views, add urls and make urls

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import ToDo
+from .serializers import ToDoSerializer
 
-# Create your views here.
+class ToDoViewSet(viewsets.ModelViewSet):
+    queryset = ToDo.objects.all().order_by('-created_at')
+    serializer_class = ToDoSerializer
+    permission_classes = [IsAuthenticated]
+
